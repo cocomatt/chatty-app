fb59d928f8f56918f7042bd0c4d56aacfe4ba539
cocomatt <matt.currie@outlook.com>
2018-11-14 19:44:22 +0000

    fixed git branch errors; everything appears to be back on track

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,15 @@
 import React, {Component} from 'react';
-<<<<<<< HEAD
 import NavBar from './NavBar.jsx';
 import Messages from './Messages.jsx';
-=======
-import MessageList from './MessageList.jsx';
->>>>>>> 563730c4e942ecca420b31e29045a56367cb10f6
 import ChatBar from './ChatBar.jsx';
-
 
 class App extends Component {
   render() {
     console.log('Rendering <App/>');
     return (
       <div>
-<<<<<<< HEAD
         <NavBar />
         <Messages />
-=======
-        <nav className="navbar">
-          <a href="/" className="navbar-brand">Chatty</a>
-        </nav>
-        <MessageList />
->>>>>>> 563730c4e942ecca420b31e29045a56367cb10f6
         <ChatBar />
       </div>
     );
